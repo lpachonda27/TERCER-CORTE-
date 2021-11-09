89fe5db40886c75f03b2cd3caaf9d0d1a6dfc0d6
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2021-11-08 21:11:41 -0500

    Solution bug library mysql

--- database/db.sql
+++ database/db.sql
@@ -9,8 +9,8 @@ CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   address VARCHAR(100) NOT NULL,
-  phone VARCHAR(15)
-  
+  phone VARCHAR(15),
+  email VARCHAR(30)
 );
 
 
